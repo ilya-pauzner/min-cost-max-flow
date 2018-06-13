aaa5c402ab2fb31faab84622f53ddb8e4d8bc4f4
ilya-the-lamer <ilya-pauzner@yandex.ru>
2018-06-13 22:31:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <set>
 
-using namespace std;
+
+using std::cin;
+using std::cout;
+using std::fixed;
+using std::make_pair;
+using std::min;
+using std::pair;
+using std::reverse;
+using std::set;
+using std::setprecision;
+using std::vector;
 
 class Graph;
 
 struct Edge {
-    int from;
-    int to;
+    size_t from;
+    size_t to;
     int weight;
     int flow;
     int capacity;
     size_t id;
 
     bool operator==(const Edge& other) const {
-        return from == other.from and to == other.to and weight == other.weight and flow == other.flow and
-               capacity == other.capacity and id == other.id;
+        return from == other.from && to == other.to && weight == other.weight && flow == other.flow &&
+               capacity == other.capacity && id == other.id;
     }
 };
 
@@ -22,18 +35,18 @@ class Graph {
 private:
     vector<Edge> edges;
     vector<int> potential;
-    int start;
-    int finish;
-    int n;
-    const int inf = (1 << 30);
-    const Edge bad_edge = {-1, -1, -1, -1, -1, 0};
+    size_t start;
+    size_t finish;
+    size_t n;
+    const unsigned int inf = (1 << 30);
+    const Edge bad_edge = {inf, inf, 0, 0, 0, inf};
     const vector<Edge> bad_edge_vector = {bad_edge};
 
-    vector<Edge> recover_path(vector<int>& d, vector<Edge>& parent) {
-        potential = d;
+    vector<Edge> recover_path(vector<int>& distances, vector<Edge>& parent) {
+        potential = distances;
         vector<Edge> answer;
-        int v = finish;
-        while (v != -1) {
+        size_t v = finish;
+        while (v != inf) {
             answer.push_back(parent[v]);
             v = parent[v].from;
         }
@@ -46,32 +59,32 @@ private:
 
     vector<Edge> ford_bellman_path(bool decomposition) {
 
-        vector<int> d(n, inf);
+        vector<int> distances(n, inf);
         vector<Edge> parent(n, bad_edge);
-        d[start] = 0;
+        distances[start] = 0;
 
         for (size_t phase = 1; phase < n; ++phase) {
             for (Edge edge : edges) {
-                if (edge.flow < edge.capacity or decomposition) {
-                    if (!decomposition or (decomposition and edge.flow > 0)) {
-                        if (d[edge.to] > d[edge.from] + edge.weight) {
+                if (edge.flow < edge.capacity || decomposition) {
+                    if (!decomposition || edge.flow > 0) {
+                        if (distances[edge.to] > distances[edge.from] + edge.weight) {
                             parent[edge.to] = edge;
-                            d[edge.to] = d[edge.from] + edge.weight;
+                            distances[edge.to] = distances[edge.from] + edge.weight;
                         }
                     }
                 }
             }
         }
 
-        return recover_path(d, parent);
+        return recover_path(distances, parent);
     }
 
     vector<Edge> dijkstra_path(bool decomposition) {
-        vector<int> d(n, inf);
+        vector<int> distances(n, inf);
         vector<Edge> parent(n, bad_edge);
         set<pair<int, int> > q;
-        d[0] = 0;
-        q.insert({d[0], 0});
+        distances[0] = 0;
+        q.insert({distances[0], 0});
         vector<vector<Edge> > gr(n);
         for (Edge edge : edges) {
             if (decomposition) {
@@ -90,24 +103,24 @@ private:
             q.erase(q.begin());
             for (auto Edge : gr[v]) {
                 int weight = Edge.weight + potential[Edge.to] - potential[Edge.from];
-                if (d[Edge.from] + weight < d[Edge.to]) {
-                    q.erase(make_pair(d[Edge.to], Edge.to));
-                    d[Edge.to] = d[Edge.from] + weight;
+                if (distances[Edge.from] + weight < distances[Edge.to]) {
+                    q.erase(make_pair(distances[Edge.to], Edge.to));
+                    distances[Edge.to] = distances[Edge.from] + weight;
                     parent[Edge.to] = Edge;
-                    q.insert(make_pair(d[Edge.to], Edge.to));
+                    q.insert(make_pair(distances[Edge.to], Edge.to));
                 }
             }
         }
 
-        return recover_path(d, parent);
+        return recover_path(distances, parent);
     }
 
 public:
-    Graph(int _start, int _finish, int _n) : start(_start), finish(_finish), n(_n) {
+    Graph(size_t _start, size_t _finish, size_t _n) : start(_start), finish(_finish), n(_n) {
         potential.resize(n);
     }
 
-    void addEdge(int v, int w, int weight, int cap) {
+    void addEdge(size_t v, size_t w, int weight, int cap) {
         Edge new_edge = {v, w, weight, 0, cap, edges.size()};
         Edge reverse_edge = {w, v, -weight, 0, 0, edges.size() + 1};
         edges.push_back(new_edge);
@@ -117,7 +130,7 @@ public:
     int flow(int flow_size) {
         int answer = 0;
         auto Path = ford_bellman_path(false);
-        while (Path != bad_edge_vector and answer < flow_size) {
+        while (Path != bad_edge_vector && answer < flow_size) {
             int diff = inf;
             for (auto edge : Path) {
                 diff = min(diff, edge.capacity - edge.flow);
@@ -161,35 +174,53 @@ public:
 
 };
 
-int main() {
+Graph read(int* k) {
     freopen("brides.in", "r", stdin);
     freopen("brides.out", "w", stdout);
-    int n, m, k;
-    cin >> n >> m >> k;
+    size_t n, m;
+    *k = 0;
+    cin >> n >> m >> *k;
     Graph G(0, n - 1, n);
     for (int i = 0; i < m; ++i) {
-        int a, b, t;
+        size_t a, b;
+        int t;
         cin >> a >> b >> t;
         --a;
         --b;
         G.addEdge(a, b, t, 1);
         G.addEdge(b, a, t, 1);
     }
-    double f = G.flow(k);
-    f /= k;
-    auto vec = G.decompose();
+    return G;
+}
+
+vector<vector<Edge> > solve(Graph* G, int k, double* flow) {
+    *flow = G->flow(k);
+    *flow /= k;
+    auto vec = G->decompose();
+    return vec;
+}
+
+void write(const vector<vector<Edge> >& answer, int k, double flow) {
     cout << fixed << setprecision(20);
 
-    if (f != -1 and vec.size() == k) {
-        cout << f << endl;
-        for (auto path : vec) {
+    if (flow != -1 && answer.size() == k) {
+        cout << flow << "\n";
+        for (auto path : answer) {
             cout << path.size() << " ";
             for (auto edge : path) {
                 cout << edge.id / 4 + 1 << " ";
             }
-            cout << endl;
+            cout << "\n";
         }
     } else {
         cout << -1;
     }
+}
+
+int main() {
+    int k;
+    Graph G = read(&k);
+    double flow;
+    vector<vector<Edge> > answer = solve(&G, k, &flow);
+    write(answer, k, flow);
 }
